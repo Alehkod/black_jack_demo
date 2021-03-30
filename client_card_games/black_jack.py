505c73c0d51e6657d5a00292a17823a4b837529c
Олег Кодик <@proektprimer.local>
2021-03-30 16:52:48 +0300

    In client mod class Player

--- client_card_games/black_jack.py
+++ client_card_games/black_jack.py
@@ -2,6 +2,7 @@ import pygame
 import socket
 import json
 import cards
+import player
 
 sock = socket.socket()
 
@@ -25,24 +26,34 @@ class Game:
     def __init__(self, name_game):
         self.name_game = name_game
         self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((1300, 700))
+        self.table_img = pygame.image.load('img/table_texture.jpg')
+        self.__shared = []
+        self.__player_cards_list = []
+        self.__player_points = 0
+        self.__dealer_cards_list = []
+        self.__dealer_points = 0
+        self.player = player.Player(self.__player_cards_list, self.__player_points, (650, 500), 'Oleg')
+        self.dealer = player.Player(self.__dealer_cards_list, self.__dealer_points, (100, 500), 'Dealer')
+        self.shared_cards = cards.SpriteCards(self.__shared, 1100, 350, 2, open_cards=False).cards
 
-    def create_game(self):
+    @staticmethod
+    def create_game():
         pygame.init()
 
     def create_screen(self):
-        self.screen = pygame.display.set_mode((1300, 700))
         pygame.display.set_caption(self.name_game)
 
-    def create_interface(self):
-        self.table_img = pygame.image.load('img/table_texture.jpg')
+    def play_game(self):
         sock.send('play'.encode("utf-8"))
-        self.shared, self.player_cards_list, self.player_points, self.dealer_cards_list, self.dealer_points = json.loads(
-            sock.recv(1024).decode('utf-8'))
-        self.shared_cards = cards.SpriteCards(self.shared, 1100, 350, 2, open_cards=False).cards
-        # self.player_cards = cards.SpriteCards(player_cards, 100, 550, 100).cards
-        # self.dealer_cards = cards.SpriteCards(dealer_cards, 100, 100, 100).cards
-        self.draw_button = cards.Button(550, 650, 'draw', black)
+        self.__receive_serv()
+        self.draw_button = cards.Button(550, 650, 'Draw', black)
         self.pass_button = cards.Button(700, 650, 'Pass', black)
+
+    def __receive_serv(self):
+        self.__shared, self.__player_cards_list, self.__player_points, \
+        self.__dealer_cards_list, self.__dealer_points = json.loads(sock.recv(1024).decode('utf-8'))
+
     def round_game(self):
         running = True
         while running:
@@ -53,42 +64,35 @@ class Game:
                     running = False
                     cmd = 'quit'
                     sock.send(cmd.encode("utf-8"))
-                    self.shared, self.player_cards_list, self.player_points, self.dealer_cards_list, self.dealer_points = json.loads(
-                        sock.recv(1024).decode('utf-8'))
+                    self.__receive_serv()
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     if self.draw_button.rect.collidepoint(*event.pos):
                         cmd = 'draw'
                         sock.send(cmd.encode("utf-8"))
-                        self.shared, self.player_cards_list, self.player_points, self.dealer_cards_list, self.dealer_points = json.loads(
-                            sock.recv(1024).decode('utf-8'))
+                        self.__receive_serv()
                     elif self.pass_button.rect.collidepoint(*event.pos):
                         cmd = 'pass'
                         sock.send(cmd.encode("utf-8"))
-                        self.shared, self.player_cards_list, self.player_points, self.dealer_cards_list, self.dealer_points = json.loads(
-                            sock.recv(1024).decode('utf-8'))
+                        self.__receive_serv()
 
-            # shared, player_cards, player_pints, dealer_cards, dealer_points = json.loads(
-            #     sock.recv(1024).decode('utf-8'))
-            point_player_result = cards.Button(100, 650, f'{self.player_points} point', red, color_fon=None)
-            point_dealer_result = cards.Button(100, 200, f'{self.dealer_points} point', red, color_fon=None)
-            self.shared_cards = cards.SpriteCards(self.shared, 1100, 350, 2, open_cards=False).cards
-            self.player_cards = cards.SpriteCards(self.player_cards_list, 100, 550, 100).cards
-            self.dealer_cards = cards.SpriteCards(self.dealer_cards_list, 100, 100, 100).cards
-            self.draw_button = cards.Button(550, 650, 'draw', black)
-            self.pass_button = cards.Button(700, 650, 'Pass', black)
-            menu_sprite = pygame.sprite.Group(self.draw_button, self.pass_button, point_player_result,
-                                              point_dealer_result)
+            sprite_point_player = cards.Button(100, 650, f'{self.player.point} point', red, color_fon=None)
+            sprite_point_dealer = cards.Button(100, 200, f'{self.dealer.point} point', red, color_fon=None)
+            menu_sprite = pygame.sprite.Group(self.draw_button, self.pass_button, sprite_point_dealer,
+                                              sprite_point_player)
+
+            self.player.list_cards_upp(self.__player_cards_list)
+            self.dealer.list_cards_upp(self.__dealer_cards_list)
             self.shared_cards.update()
-            self.player_cards.update()
-            self.dealer_cards.update()
+            # self.player.sprite_cards.update()
+            # self.dealer.sprite_cards.update()
             pygame.display.update()
 
             self.screen.blit(self.table_img, (0, 0))
             menu_sprite.draw(self.screen)
             self.shared_cards.draw(self.screen)
-            self.player_cards.draw(self.screen)
-            self.dealer_cards.draw(self.screen)
-            # sock.send(''.encode("utf-8"))
+            self.player.sprite_cards.draw(self.screen)
+            self.dealer.sprite_cards.draw(self.screen)
+
     def quit(self):
         pygame.quit()
 
@@ -96,6 +100,6 @@ class Game:
 black_jack = Game('Black_Jack')
 black_jack.create_game()
 black_jack.create_screen()
-black_jack.create_interface()
+black_jack.play_game()
 black_jack.round_game()
 black_jack.quit()
--- client_card_games/player.py
+++ client_card_games/player.py
@@ -1,14 +1,25 @@
-import deckCards
+import cards
 import random
+import pygame
 
 name_list = ['Alex', 'Anthony', 'Brandon', 'Christopher', 'David', 'Dillon', 'Ethan', 'Fred', 'Josh',
              'Justin', 'Kevin', 'Ryan', 'Thomas', 'Tyler', 'William']
 
 
-class Player:
+class Player(pygame.sprite.Sprite):
     def __init__(self, list_cards, point, pos_cards, name=random.choice(name_list), step_cards=100):
+        pygame.sprite.Sprite.__init__(self)
         self.name = name
         self.point = point
-        self.card_list = list_cards
-        self.pos_cards = pos_cards
-        self.step_cards = step_cards
+        self.__list_cards = list_cards
+        self.__pos_cards = pos_cards
+        self.__step_cards = step_cards
+        self.sprite_cards = self.__sprite_cards()
+
+    def __sprite_cards(self):
+        return cards.SpriteCards(self.__list_cards, self.point, *self.__pos_cards, open_cards=True).cards
+
+    def list_cards_upp(self, args):
+        self.__list_cards = args
+        self.sprite_cards = self.__sprite_cards()
+        self.sprite_cards.update()
